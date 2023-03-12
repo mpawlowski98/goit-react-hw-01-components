ab2653864e188e0b95a348e4a58581c06a6bf510
Michał Pawłowski <107926477+mpawlowski98@users.noreply.github.com>
2023-03-12 18:49:31 +0100

    Upp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import user from './user.json';
+
+import { Profile } from './Task1/Profile';
+
 export const App = () => {
   return (
     <div
@@ -7,7 +11,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
---
+++ src/components/Task1/Profile.jsx
@@ -0,0 +1,14 @@
+import css from './Profile.module.css';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img className={css.avatar} alt={css.username} src={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+    </div>
+  );
+};
